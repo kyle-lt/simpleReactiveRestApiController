06bb3c30ef31a6c48e8e4957aa23f4130ad02e30
Kyle Tully <kjtully@yahoo.com>
2021-06-07 14:54:13 -0500

    first commit

---
+++ src/main/java/com/ktully/otel/simpleReactiveRestApiController/SimpleReactiveRestApiControllerApplication.java
@@ -0,0 +1,13 @@
+package com.ktully.otel.simpleReactiveRestApiController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReactiveRestApiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReactiveRestApiControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktully/otel/simpleReactiveRestApiController/SimpleReactiveRestApiControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktully.otel.simpleReactiveRestApiController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleReactiveRestApiControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
